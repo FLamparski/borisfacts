8ee314a908329a5ee3c3fd01b4e0d57ac32eded2
Filip Wieland <filiplamparski@gmail.com>
2016-07-02 11:07:42 +0200

    Trololol

--- borisfacts/settings.py
+++ borisfacts/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k#7aq0lq78v8i-@z=5ep*i@)0+034o*3wc7h%&!v7an_*+nl#+'
+SECRET_KEY = 'A_BAD_SECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
